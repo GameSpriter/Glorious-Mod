ae3e67583b6fb78aae672f6e9e748134437f86f5
GameSpriter <GameSpriter@gmail.com>
2016-07-19 15:20:24 -0400

    I don't remember what I did last night

--- src/main/java/com/GameSpriter/Glorious/blocks/BlockShinySapling.java
+++ src/main/java/com/GameSpriter/Glorious/blocks/BlockShinySapling.java
@@ -5,42 +5,30 @@ import java.util.Random;
 import com.GameSpriter.Glorious.Reference;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockLeaves;
-import net.minecraft.block.BlockOldLeaf;
-import net.minecraft.block.BlockOldLog;
+import net.minecraft.block.BlockDirt;
+import net.minecraft.block.BlockFlower;
 import net.minecraft.block.BlockPlanks;
-import net.minecraft.block.BlockSapling;
-import net.minecraft.block.IGrowable;
-import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenBigTree;
-import net.minecraft.world.gen.feature.WorldGenBirchTree;
-import net.minecraft.world.gen.feature.WorldGenCanopyTree;
-import net.minecraft.world.gen.feature.WorldGenMegaJungle;
-import net.minecraft.world.gen.feature.WorldGenMegaPineTree;
-import net.minecraft.world.gen.feature.WorldGenSavannaTree;
-import net.minecraft.world.gen.feature.WorldGenTaiga2;
 import net.minecraft.world.gen.feature.WorldGenTrees;
 import net.minecraft.world.gen.feature.WorldGenerator;
 
-public class BlockShinySapling extends BlockSapling {
+public class BlockShinySapling extends BlockFlower {
 
-	public static final PropertyEnum<BlockPlanks.EnumType> TYPE = PropertyEnum.<BlockPlanks.EnumType>create("type", BlockPlanks.EnumType.class);
-    public static final PropertyInteger STAGE = PropertyInteger.create("stage", 0, 1);
-    
-	
 	public BlockShinySapling() {
 		setUnlocalizedName(Reference.GloriousBlocks.SHINY_SAPLING.getUnlocalizedName());
 		setRegistryName(Reference.GloriousBlocks.SHINY_SAPLING.getRegistryName());
 		
 	}
+	
+	public static boolean growableOnBlock(Block block) {
+		return block == net.minecraft.init.Blocks.DIRT || block == net.minecraft.init.Blocks.GRASS;
+	}
 
-	@Override
 	public void generateTree(World worldIn, BlockPos pos, IBlockState state, Random rand) {
 		if (!net.minecraftforge.event.terraingen.TerrainGen.saplingGrowTree(worldIn, rand, pos))
 			return;
@@ -48,6 +36,11 @@ public class BlockShinySapling extends BlockSapling {
 				: new WorldGenTrees(true));
 		
 		System.out.println("Tree Spawned(temp placeholder)");
+	}
+
+	@Override
+	public EnumFlowerColor getBlockType() {
+		return null;
 	}
 
 }
